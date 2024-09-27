4f22400711680f2dba2a055f1c76a9585d93431c
Kai <74490542+Leokayasen@users.noreply.github.com>
2024-09-27 22:49:34 +0100

    Create options.py

---
+++ options.py
@@ -0,0 +1,49 @@
+from gameMenu import runMenu
+
+
+def option1():
+  print("You have selected option 1")
+  print("Starting Game")
+  print("Loading...")
+  print("Loading...")
+  print("Loading...")
+  print("Loading...")
+  runMenu()
+
+
+option1()
+
+
+def option2():
+  print("You have selected option 2")
+  print("Help")
+  print("What do you want me to do, bake you a cake?")
+
+
+option2()
+
+
+def option3():
+  print("You have selected option 3")
+  print("""
+	--
+	
+	CREDITS
+	Developer:
+
+	[This Program was originally made in 2020, and re-built in 2024]
+
+	--
+	""")
+
+
+option3()
+
+
+def option4():
+  print("You have selected option 4")
+  print("Exiting...")
+  exit
+
+
+option4()
